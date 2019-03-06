ca1b9630b33dad258c9ae15001d215216f0ec84b
Greg <greggordon98@gmail.com>
2019-03-06 10:26:29 -0800

    start
    start

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
@@ -8,7 +6,6 @@ class App extends Component {
       <div className="App">
         {/* <Header /> */}
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -21,6 +18,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        {/* Header / */}
       </div>
     );
   }
