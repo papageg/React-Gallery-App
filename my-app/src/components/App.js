3a00edb291b332288d3103af7978b3fd7dbe085f
Greg <greggordon98@gmail.com>
2019-03-13 14:40:22 -0700

    lost
    lost

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -41,6 +41,16 @@ class App extends Component {
       });
   }
 
+  hikingSearch = () => {
+    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=hiking&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({ photo: responseData.photos.photo });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  }
 
   render() {
     console.log(this.state);
--- my-app/src/components/Hiking.js
+++ my-app/src/components/Hiking.js
@@ -5,20 +5,8 @@ const api = apiKey;
 
 class Hiking extends Component {    
 
-  componentDidMount() {
-    this.hikingSearch();
-  }
-  
-  hikingSearch = () => {
-    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=hiking&per_page=24&format=json&nojsoncallback=1`)
-      .then(response => response.json())
-      .then(responseData => {
-        this.setState({ photo: responseData.photos.photo });
-      })
-      .catch(error => {
-        console.log('Error fetching and parsing data', error);
-      });
-  }
+
+
 
    render() {
      return (
