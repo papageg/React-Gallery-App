5f5151fcedd6de4a1b43f147050b2e5a40e38999
Greg <greggordon98@gmail.com>
2019-03-14 10:09:52 -0700

    losty
    losty

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -7,12 +7,11 @@ import Header from './Header';
 import Footer from './Footer';
 import apiKey from "./Config.js";
 import Home from './Home';
-import Soccer from './Soccer';
-import Hiking from './Hiking';
-import Food from './Food';
+//import Soccer from './Soccer';
+//import Hiking from './Hiking';
+//import Food from './Food';
 import NotFound from "./NotFound";
 import Gallery from "./Gallery";
-import { isNumberTypeAnnotation } from 'babel-types';
 
 const api = apiKey;
 //const flkrImages = [];
@@ -22,21 +21,33 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      photo: [],
-      //query: input.value
+      photo: []
     };
   }
 
   componentDidMount() {
+    this.generalSearch();
     this.soccerSearch();
-    
+    this.hikingSearch();
+    this.foodSearch();
   }
   
+  generalSearch = () => {
+    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=general&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({ general: responseData.photos.photo });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  }
+
   soccerSearch = () => {
     fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=soccer&per_page=24&format=json&nojsoncallback=1`)
       .then(response => response.json())
       .then(responseData => {
-        this.setState({ photo: responseData.photos.photo });
+        this.setState({ soccer: responseData.photos.photo });
       })
       .catch(error => {
         console.log('Error fetching and parsing data', error);
@@ -47,12 +58,24 @@ class App extends Component {
     fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=hiking&per_page=24&format=json&nojsoncallback=1`)
       .then(response => response.json())
       .then(responseData => {
-        this.setState({ photo: responseData.photos.photo });
+        this.setState({ hiking: responseData.photos.photo });
       })
       .catch(error => {
         console.log('Error fetching and parsing data', error);
       });
   }
+
+  foodSearch = () => {
+    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=food&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({ food: responseData.photos.photo });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  }
+
 
   render() {
     console.log(this.state);
@@ -62,10 +85,10 @@ class App extends Component {
         <div className="container">
         <Header />
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/soccer" component={Soccer} /> 
-            <Route exact path="/hiking" component={Hiking} />
-            <Route path="/food" component={Food} />
+            <Route exact path="/" render={ () => <Gallery images={this.state.general} title={'General'} />} />
+            <Route path="/soccer" render={ () => <Gallery images={this.state.soccer} title={'Soccer'} />} /> 
+            <Route exact path="/hiking" render={ () => <Gallery images={this.state.hiking} title={'Hiking'} />} />
+            <Route path="/food" render={ () => <Gallery images={this.state.food} title={'Food'} />} />
             <Route component={NotFound}/>
           </Switch> 
           {/* Pictures Here */}
