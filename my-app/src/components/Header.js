1331765144f0b08153ad151fe18455bbd0cf7611
Greg <greggordon98@gmail.com>
2019-03-14 12:32:00 -0700

    hmm
    hmm

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import logo from '../images/ggw.png';
+import { Link } from 'react-router-dom';
 
-const Header = (props) => (
-     
+class Header extends Component {
     
-    <header>
-        <span className='ggw-logo'><img src={logo} alt="ggw logo"></img></span>
+    render () {
+        return (
+            <header>
+                <span className='ggw-logo'><img src={logo} alt="ggw logo"></img></span>
 
-        <form>
-            <input type="text" name="text"></input>
-            <input type="submit" name="search" onCLick={props.search}></input>
-        </form>
+                <form>
+                    <input type="text" name="text"></input>
+                    <input type="submit" name="search"></input>
+                    
+                </form>
 
-        <button>Home</button>
-        <button>Soccer</button>
-        <button>Hiking</button>
-        <button>Food</button>
+                
+                <Link to="/"><button>Home</button></Link>
+                <Link to="/soccer"><button>Soccer</button></Link>
+                <Link to="/hiking"><button>Hiking</button></Link>
+                <Link to="/food"><button>Food</button></Link>
 
-    </header>
-);
-
+            </header>
+        )
+    }
+    
+};
 export default Header;
+
+
+class Home extends Component {    
+    render() {
+      return (
+       <h2>Home Page</h2>
+     )
+   }
+ };
