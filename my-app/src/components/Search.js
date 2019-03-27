48d2940d9376be0b2a02e6e87f180d3cd8d60182
Greg <greggordon98@gmail.com>
2019-03-27 09:07:01 -0700

    broke
    broke

--- my-app/src/components/Search.js
+++ my-app/src/components/Search.js
@@ -12,11 +12,13 @@ class Search extends Component {
  }
 
  getInfo = () => {
-<<<<<<< HEAD
     fetch(`{https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=${this.state.query}&per_page=24&format=json&nojsoncallback=1}`)
-=======
+
     axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=${this.state.query}&per_page=24&format=json&nojsoncallback=1`)
->>>>>>> parent of 60ed4ce... dad lost
+
+
+    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api}&tags=${this.state.query}&per_page=24&format=json&nojsoncallback=1`)
+
     .then(response => response.json())
     .then(responseData => {
         this.setState({ results: responseData.photos.photo });
@@ -41,10 +43,9 @@ class Search extends Component {
   }
 
  render() {
-<<<<<<< HEAD
+
    console.log(this.state.query)
-=======
->>>>>>> parent of 60ed4ce... dad lost
+
    return (
     <div>
         <span className='ggw-logo'><img src={logo} alt="ggw logo"></img></span>
@@ -54,16 +55,16 @@ class Search extends Component {
             ref={input => this.search = input}
             onSubmit={this.handleInputChange}
         />
-<<<<<<< HEAD
+
         <button type='submit' onClick={this.handleInputChange}>Submit</button>
         </form>
         <Switch>
         <Route path={this.state.query} render={ () => <Gallery photos={this.state.results} title={this.state.query} />} />
         </Switch>
-=======
+
         <Suggestions results={this.state.results} />
         </form>
->>>>>>> parent of 60ed4ce... dad lost
+
      </div>
    );
  }
